0b8cbafae67f2ab2506c10402004c304b451ff96
Dom Rodriguez <shymega@shymega.org.uk>
2024-03-27 20:02:32 +0000

    refactor: Refactor build tooling
    - Refactor devenv
    - Use env var or Gradle 'ext' for using external android.jar
    - Adjust CI

--- devenv.nix
+++ devenv.nix
@@ -1,21 +1,26 @@
 { pkgs, ... }:
-
 {
-  packages = (with pkgs.androidSdkPackages; [
-    build-tools-34-0-0
-    cmdline-tools-latest
-    emulator
-    platform-tools
-    platforms-android-34
-  ]) ++ (with pkgs.androidStudioPackages; [
-    stable
-  ]);
+  packages = with pkgs; [
+    androidSdkPackages.cmdline-tools-latest
+    androidSdkPackages.emulator
+    androidSdkPackages.platform-tools
+    androidStudioPackages.stable
+  ];
+  
+  enterShell = ''
+    export ANDROID_SDK_ROOT="$HOME/Android"
+    export ANDROID_HOME="$HOME/Android"
+    export PATH="$PATH:$ANDROID_HOME/Sdk/tools/bin:$ANDROID_HOME/Sdk/platform-tools"
+    export IS_OUTSIDE_BSP="true"
+  '';
 
   languages = {
     nix.enable = true;
     java = {
       enable = true;
+      jdk.package = pkgs.jdk21;
       gradle.enable = true;
     };
   };
+
 }
